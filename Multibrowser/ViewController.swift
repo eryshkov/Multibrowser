b0f06270ed0d6b85b42eab9d7f7cf22459f3f14d
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-02 12:24:10 +0300

    addWebView() added

--- Multibrowser/ViewController.swift
+++ Multibrowser/ViewController.swift
@@ -7,16 +7,57 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    //MARK: -
     @IBOutlet weak var addressBar: UITextField!
     @IBOutlet weak var stackView: UIStackView!
     
+    //MARK: -
+    func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+    
+    @objc func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        
+        webView.load(URLRequest(url: url))
+    }
+    
+    @objc func deleteWebView() {
+        
+    }
+    
+    //MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        
+        navigationItem.rightBarButtonItems = [delete, add]
     }
 
 
 }
+//MARK: -
+extension ViewController: WKNavigationDelegate {
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    
+}
