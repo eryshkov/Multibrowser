4af942b291e654f1996e45ef7e0e44aee48e2640
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-02 11:36:33 +0300

    Initial Commit

---
+++ Multibrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Evgeniy Ryshkov on 02/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
