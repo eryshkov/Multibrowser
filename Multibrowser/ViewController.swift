1295216eedba526146a8061268785cbbc35b0557
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-02 12:04:20 +0300

    constraints and delegate added

--- Multibrowser/ViewController.swift
+++ Multibrowser/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
